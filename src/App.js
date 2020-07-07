1067fefb6d193a2b5b26e943967f6c9bb533c55b
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-07-07 09:38:08 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      React.createElement("div", {
+        className: "shopping-list"
+      }, 
+      /*#__PURE__*/React.createElement("h1", null, "Shopping List for ", this.props.name), 
+      /*#__PURE__*/React.createElement("ul", null, 
+      /*#__PURE__*/React.createElement("li", null, "Instagram"), 
+      /*#__PURE__*/React.createElement("li", null, "WhatsApp"), 
+      /*#__PURE__*/React.createElement("li", null, "Oculus"))
+      ));
+  }
 }
-
-export default App;
+export default ShoppingList;
