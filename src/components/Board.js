638a4eb97058c5e8b05067526101f98d6d31455a
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-12-16 16:47:31 -0300

    finished modularization

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Square from "./Square";
 const Board = props => {    
-  //console.log(props)
+  console.log(props)
   const renderSquare = i => {
     return (
       <Square
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Square = props => {
-  //console.log(props)
+  console.log(props)
   return (
     <button
       className="square"
